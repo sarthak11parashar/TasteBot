b9d4b7ff6e4c540586bb076b7ac59e28e4ed3d16
sarthak11parashar <115147509+sarthak11parashar@users.noreply.github.com>
2025-08-30 01:53:13 +0530

    Update ClaudeRecipe.jsx

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown"
 export default function ClaudeRecipe(props) {
     return (
         <section className=".suggested-recipe-container">
-            <h2>Chef Claude Recommends:</h2>
+            <h2>Taste Bot Recommends:</h2>
             <ReactMarkdown>{props.recipe}</ReactMarkdown>
         </section>
     )
