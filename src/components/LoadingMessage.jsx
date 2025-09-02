43165f8b4546d6646e381591cb544052e70dcf65
sarthak11parashar <115147509+sarthak11parashar@users.noreply.github.com>
2025-09-02 16:50:29 +0530

    Create LoadingMessage.jsx

---
+++ src/components/LoadingMessage.jsx
@@ -0,0 +1,7 @@
+export default function LoadingMessage() {
+    return (
+        <p className="loading-message">
+            🍳 Hold on while Taste Bot is getting you a recipe<span className="dots"></span>
+        </p>
+    )
+}
